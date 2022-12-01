9d4d7ed9ff7bb911d2aa730a9240540a6ef7bd80
Muhammad Nauman Raza <thedevraza@proton.me>
2022-12-01 18:26:06 +0000

    feat: battery notifications

--- utilities/notifications.lua
+++ utilities/notifications.lua
@@ -55,3 +55,33 @@ local mpd =
         }
     end
 }
+
+-- Battery Notifications
+local battery =
+    lain.widget.bat {
+    settings = function()
+        widget:set_markup('<span color="' .. beautiful.text .. '"><b>' .. bat_now.perc .. "</b></span>")
+        bat_notification_charged_preset = {
+            border_color = beautiful.bg_normal,
+            bg = beautiful.bg_normal,
+            timeout = 5,
+            title = '<span color="' .. beautiful.positive .. '"><b>Battery full charged!</b></span>',
+            text = "You can now unplug the cable."
+        }
+        bat_notification_low_preset = {
+            border_color = beautiful.bg_normal,
+            bg = beautiful.bg_normal,
+            timeout = 5,
+            title = '<span color="' .. beautiful.negative .. '"><b>Battery low!</b></span>',
+            text = "Plug in the cable."
+        }
+        bat_notification_critical_preset = {
+            border_color = beautiful.bg_normal,
+            timeout = 5,
+            bg = beautiful.bg_normal,
+            title = '<span color="' .. beautiful.negative .. '"><b>Battery critical!</b></span>',
+            text = "Impending doom approaches!."
+        }
+    end,
+    batteries = {"BAT1", "BAT2"}
+}
