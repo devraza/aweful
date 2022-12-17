25994a006f2e349cec0fa178812dbead3b3cb1c4
Muhammad Nauman Raza <thedevraza@proton.me>
2022-12-17 20:54:54 +0000

    feat: clean up autostart utility

--- utilities/autostart.lua
+++ utilities/autostart.lua
@@ -3,4 +3,3 @@ local gears = require("gears")
 
 --- Autostart ---
 awful.spawn.with_shell(gears.filesystem.get_configuration_dir() .. "utilities/scripts/startup.sh")
-awful.spawn.with_shell(gears.filesystem.get_configuration_dir() .. "utilities/scripts/monitors.sh")
--- utilities/scripts/startup.sh
+++ utilities/scripts/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export SCRIPTPATH="$(
+  cd "$(dirname "$0")" || exit
+  pwd -P
+)"
+
+$SCRIPTPATH/monitors.sh
 emacs --daemon &
 pipewire &
 pipewire-pulse &
