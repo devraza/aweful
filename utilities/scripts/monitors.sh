a79603ed4ba8e9fff216af167f579f0814331e7b
Muhammad Nauman Raza <thedevraza@proton.me>
2022-12-21 17:02:31 +0000

    chore: update scripts

--- utilities/scripts/monitors.sh
+++ utilities/scripts/monitors.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 2560x1080 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 2560x1080 --pos 0x0 --rotate normal
--- utilities/scripts/startup.sh
+++ utilities/scripts/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-picom &
-
 export SCRIPTPATH="$(
   cd "$(dirname "$0")" || exit
   pwd -P
 )"
 
+picom &
+pipewire &
+pipewire-pulse &
 emacs --daemon &
-pulseaudio &
 mpd &
 $SCRIPTPATH/monitors.sh
 alacritty --class spad -e tiny &
