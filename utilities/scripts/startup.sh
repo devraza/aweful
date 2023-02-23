0b0f14fe0c31e7e3c78416cb24acedf9c92078f5
Muhammad Nauman Raza <thedevraza@proton.me>
2023-02-23 10:39:15 +0000

    chore: use Awesome for wallpapers instead of feh

--- utilities/scripts/startup.sh
+++ utilities/scripts/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-feh --bg-fill ~/.config/awesome/assets/wallpapers/graphite-ultrawide.png
 pkill redshift
-pkill redshift
+pkill picom
+pkill mpd
 redshift -l 52.486244:-1.890401 &
 picom &
 emacs --daemon &
